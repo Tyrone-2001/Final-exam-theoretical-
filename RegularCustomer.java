8ccd12cb78b3c1ee61d8c369e5076f0e35ea69b6
Tyrone-2001 <116233795+Tyrone-2001@users.noreply.github.com>
2023-01-07 21:57:17 +0800

    Create RegularCustomer.java

---
+++ RegularCustomer.java
@@ -0,0 +1,23 @@
+public class RegularCustomer extends Customer 
+
+ { 
+
+          
+
+         double calculateBill(){ 
+
+                 return amount; 
+
+         } 
+
+          
+
+         RegularCustomer(String name, double amount){ 
+
+                 super(name, amount); 
+
+         } 
+
+ 
+
+ }
